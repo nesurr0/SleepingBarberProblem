b383e576e78a5649055d03e8714c1f66c9fe5100
nesurr <sotva000@mail.ru>
2021-12-12 02:05:41 +0300

    Скелет проекта

--- src/base/BarberShop.java
+++ src/base/BarberShop.java
@@ -3,13 +3,85 @@ package base;
 import java.util.concurrent.Semaphore;
 
 public class BarberShop {
+    private int numberOfFreeSeats;
+    private Semaphore barberSem;
+    private Semaphore customerSem;
+    private Semaphore accessSeatsSem;
+    public class Customer implements Runnable{
+
+        final private String name;
+        Customer(String name){
+            this.name = name;
+        }
+        Customer(){
+            name = "Парикмахер";
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                accessSeatsSem.acquire(); // Только один поток имеет доступ
+                if(numberOfFreeSeats > 0) {
+                    numberOfFreeSeats--; // декремент
+                    customerSem.release(); // будим парикмахера
+                    accessSeatsSem.release(); // освобождаем место
+                    numberOfFreeSeats++;
+                    accessSeatsSem.release(); // отдаем мьютекс доступа к посадке
+                    barberSem.acquire(); // ждем открытия семафора ждем стрижки
+                    // стригут клиента
+                } else {
+                    accessSeatsSem.release(); // отдаем мьютекс
+                    // уходим
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    public class Barber implements Runnable{
+
+        final private String name;
+        Barber(String name){
+            this.name = name;
+        }
+
+        Barber(){
+            name = "Парикмахер";
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                customerSem.acquire(); // Спит пока не придет customer
+                accessSeatsSem.acquire(); //wait Одноместного семафора для блокировки изменения состояния доступных клиентов
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            barberSem.release(); // ждать семафор клиента для подстрижки
+            accessSeatsSem.release(); // отдать мьютекс доступа к посадке
+            //System.out.println(Парикмахер стрижет клиента);
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        System.out.println("hello," + args[0]);
         int MAX_COUNT_SITS = 6;
-        Semaphore customers_wait_semaphore = new Semaphore(MAX_COUNT_SITS,true);
+        Semaphore customers_semaphore = new Semaphore(MAX_COUNT_SITS,true);
         Semaphore barber_semaphore = new Semaphore(1,true);
-        Semaphore accessSeats = new Semaphore(1,true);//mutex
+        Semaphore accessSeats_semaphore = new Semaphore(1,true);//mutex
         int count_customers;
+        int numberOfFreeSeats;
     }
 }
